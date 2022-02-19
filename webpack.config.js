1c5daf01c2946857f6bb3ca97e1225eede8fe6f1
yechan <baesku789@gmail.com>
2022-02-19 11:24:00 +0900

    [master] - dev server 추가 - 환경변수 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const dotenv = require('dotenv')
+
+//환경변수 세팅
+dotenv.config({path: './.env'})
 
 module.exports = {
-    entry: "./src/client.js",
+    entry: {
+        main :"./src/client.js",
+    },
+
+    devServer: {
+        hot : true,
+        port : "9000",
+    },
+
     output: {
         path: path.resolve(__dirname, "build"), //required absolute path
         filename: "[name].bundle.js"
@@ -20,11 +33,25 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "public/template/index.ejs",
 
+            // minify: true,
+
+            meta: {
+                'description': 'To Do List'
+            },
+
             //사용자 정의 옵션(index.ejs 파일에 넘겨줌)
             templateParameters: {
                 lang : "ko",
                 title : "To Do"
             }
+        }),
+
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*',
+                //clean build folder
+                path.resolve(__dirname, 'build/**/*')
+            ]
         })
     ]
 }
