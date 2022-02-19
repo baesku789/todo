ba1f0a09f7d389fb59cdc0af2ac026ec5540c770
yechan <baesku789@gmail.com>
2022-02-19 22:42:49 +0900

    aa

---
+++ src/client.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ReactDom = require("react-dom");
+ReactDom.render(/>, document.getElementById('root')););
+//# sourceMappingURL=client.js.map
---
+++ src/page/components/Home.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = require("react");
+const home_scss_1 = require("./home.scss");
+const Home = () => {
+    return (react_1.default.createElement("div", { className: home_scss_1.default.home }, "Home"));
+};
+exports.default = Home;
+//# sourceMappingURL=Home.js.map
