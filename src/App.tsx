a2ac5b163ba36e2cdf65556f119ec9a7384f4293
yechan <baesku789@gmail.com>
2022-02-21 20:50:53 +0900

    [master] - global styles 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
 import {Root} from "./page/Root";
 import {BrowserRouter} from "react-router-dom";
+import {createGlobalStyle} from "styled-components";
 
 export const App = () => {
+    const GlobalStyles = createGlobalStyle`
+      body{
+        box-sizing: border-box
+      }
+    `
+
     return (
         <BrowserRouter>
+            <GlobalStyles/>
             <Root/>
         </BrowserRouter>
     )
--- src/page/components/home/Home.tsx
+++ src/page/components/home/Home.tsx
@@ -6,7 +6,11 @@ export const Home = () => {
 
     return (
         <>
-            <div className={styles.home}>로그인</div>
+            <h2 className={styles.home}>로그인</h2>
+            <h3>아이디</h3>
+            <input type="text"/>
+            <h3>비밀번호</h3>
+            <input type="text"/>
         </>
     )
 }
