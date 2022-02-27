648a607528c471e98d7ce18d45b7792e250d5f9e
yechan <baesku789@gmail.com>
2022-02-27 21:25:42 +0900

    [master] - 할일 추가 기능 구현

--- src/page/components/todo/Todo.tsx
+++ src/page/components/todo/Todo.tsx
@@ -6,16 +6,27 @@ import {v4 as uuidv4} from 'uuid'
 interface todoState {
     id: string
     text: string,
-    date : number
+    date : string,
+    done : boolean
 }
 
 const Todo = () => {
     const [todoList, setTodoList] = useState<todoState[]>([]);
+    const [text, setText] = useState("");
 
     const date = new Date();
+    const getMonthDay = `${date.getMonth() + 1}/${date.getDate()}`
 
-    const addTodo = (text: string) => {
-        setTodoList(state => [...state, {text : text, date: date.getDate(), id: uuidv4()}])
+    const addTodo = (e: React.KeyboardEvent<HTMLInputElement>) : void => {
+        if(e.key === "Enter"){
+            setTodoList(state => [...state, {id: uuidv4(), text: text, date: getMonthDay, done: false}])
+            setText("")
+        }
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {value} = e.target;
+        setText(value)
     }
 
     return (
@@ -25,6 +36,14 @@ const Todo = () => {
             </div>
 
             <div className={styles.scrollBox}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    value={text}
+                    placeholder={"할 일을 입력하세요."}
+                    onKeyPress={(e) => addTodo(e)}
+                    onChange={e => onChange(e)}
+                />
                 {
                     todoList.map((todo) => {
                       return (
@@ -32,16 +51,16 @@ const Todo = () => {
                               className={styles.todoList}
                               key={todo?.id}
                           >
-                              <div>{todo?.text}</div>
-                              <div>{todo?.date}</div>
+                              <div>{todo.text}</div>
+                              <div className={styles.date}>{todo?.date}</div>
                           </div>
                       )
                     })
                 }
-                <div className={styles.plus_circle} onClick={() => addTodo("aa")}>
-                    <div className={styles.plus_1}/>
-                    <div className={styles.plus_2}/>
-                </div>
+                {/*<div className={cn(styles.plus_circle, inputDisplay && styles.x_btn)} onClick={() => setInputDisplay(!inputDisplay)}>*/}
+                {/*    <div className={styles.plus_1}/>*/}
+                {/*    <div className={styles.plus_2}/>*/}
+                {/*</div>*/}
             </div>
         </div>
     )
