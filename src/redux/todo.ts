098531c0a50f7ff1aed85a4ae5174370282c243e
yechan <baesku789@gmail.com>
2022-03-01 10:57:04 +0900

    [typesafe-actions] - typesafe-actions reducer

--- src/redux/todo.ts
+++ src/redux/todo.ts
@@ -1,32 +1,20 @@
 import {v4 as uuidv4} from 'uuid'
-
-//as const로 지정하면 type이 string이 아닌 'redux/*'로 잡힘
-const CREATE = 'redux/CREATE' as const;
-const TOGGLE = 'redux/TOGGLE' as const;
-const REMOVE = 'redux/REMOVE' as const;
+import {ActionType, createAction, createReducer} from "typesafe-actions";
 
 //액션 생성 함수 선언
-export const create = (text: string) => ({
-    type: CREATE,
-    payload: text
-})
-
-export const toggle = (id: string) => ({
-    type: TOGGLE,
-    payload: id
-})
-
-export const remove = (id: string) => ({
-    type: REMOVE,
-    payload: id
-})
+export const create = createAction('redux/CREATE')<string>();
+export const toggle = createAction('redux/TOGGLE')<string>();
+export const remove = createAction('redux/REMOVE')<string>();
 
 //모든 액션 객체들에 대한 타입을 준비
 // ReturnType<typeof _____> 는 특정 함수의 반환값을 추론
-type TodoAction =
-    | ReturnType<typeof create>
-    | ReturnType<typeof toggle>
-    | ReturnType<typeof remove>
+const actions = {
+    create,
+    toggle,
+    remove
+}
+
+type TodoAction  = ActionType<typeof actions>
 
 type Todo = {
     id: string,
@@ -44,26 +32,13 @@ const initialState = [
     }
 ]
 
-function todo(
-    state : TodoState = initialState,
-    action : TodoAction
-){
-    switch (action.type){
-        case "redux/CREATE":
-            return state.concat({
-                id: uuidv4(),
-                text: action.payload,
-                done: false
-            });
-        case "redux/TOGGLE":
-            return state.map((todo) => (
-                todo.id === action.payload ? {...todo, done: !todo.done} : todo
-            ))
-        case "redux/REMOVE":
-            return state.filter((todo) => todo.id !== action.payload)
-        default :
-            return state;
-    }
-}
+const todoReducer = createReducer<TodoState, TodoAction>(initialState)
+    .handleAction(create, (state, action) => state.concat({
+        id: uuidv4(),
+        text: action.payload,
+        done: false
+    }))
+    .handleAction(toggle, (state, action) => state.map((todo) => todo.id === action.payload ? {...todo, done: !todo.done}: todo))
+    .handleAction(remove, (state, action) => state.filter((todo) => todo.id !== action.payload))
 
-export default todo;
+export default todoReducer;
